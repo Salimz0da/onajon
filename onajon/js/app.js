d06e6e56c28673241ce35df2a39504d35058029c
Salimzoda <119676998+Salimz0da@users.noreply.github.com>
2023-07-29 01:58:40 +0500

    Add files via upload

---
+++ onajon/js/app.js
@@ -0,0 +1,21 @@
+alert("Поздровление для моей мамы");
+const buttonElement = document.getElementById("#btn");
+
+// console.log(buttonElement);
+
+function hello(){
+    alert("Поздоровляю ")
+    alert("Вас")
+    alert("С")
+    alert("Днем")
+    alert("Рождения")
+    alert("Мама")
+    alert("❤️❤️❤️❤️")
+
+
+    alert("Я вас люблю")
+    alert("Я вас люблю")
+    alert("Я вас люблю")
+    alert("Я вас люблю")
+    alert("Я вас люблю")
+}
